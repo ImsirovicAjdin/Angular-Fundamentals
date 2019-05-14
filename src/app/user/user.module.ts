ff370e966d2b1021a1840233bd00e0b3a4b05efd
FlowOverstack <ajdika@live.com>
2019-05-14 14:15:15 +0200

    7.3, Creating your first template-based form, pt2

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { RouterModule } from '@angular/router'
+import { FormsModule } from '@angular/forms'
 
 import { userRoutes } from './user.routes'
 import { ProfileComponent } from './profile.component'
@@ -9,7 +10,8 @@ import { LoginComponent } from './login.component'
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule.forChild(userRoutes)
+    RouterModule.forChild(userRoutes),
+    FormsModule
   ],
   declarations: [
     ProfileComponent,
