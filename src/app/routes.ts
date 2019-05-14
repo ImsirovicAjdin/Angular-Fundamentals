d69fb479e8fd1fc106bf7c6de4153b5117cbb0b7
FlowOverstack <ajdika@live.com>
2019-05-14 11:31:05 +0200

    Section 6.6 Navigating from code (using click event binding and this.router.navigate(['events']) combination"

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router' // this adds TypeScript type definition and compile-time safety so we can't type paht typo without being warned about it
 import { EventsListComponent } from './events/events-list.component'
 import { EventDetailsComponent } from './events/event-details/event-details.component'
+import { CreateEventComponent } from './events/create-event.component';
 
 
 export const appRoutes:Routes = [
+  { path: 'events/new', component: CreateEventComponent },    
   { path: 'events', component: EventsListComponent },
   { path: 'events/:id', component: EventDetailsComponent }, // /events/1 or /events/foo
   { path: '', redirectTo: '/events', pathMatch: 'full' }
