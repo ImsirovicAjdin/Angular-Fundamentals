e55bfed1f82074e3e782cc4c1c5953f5fde9614f
FlowOverstack <ajdika@live.com>
2019-05-14 13:25:16 +0200

    6.12 Organizing Your Exports with Barrels

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
+import { RouterModule, ActivatedRouteSnapshot } from '@angular/router'
 
-import { EventsAppComponent } from './events-app.component';
-import { EventsListComponent } from './events/events-list.component'
-import { EventThumbnailComponent } from './events/event-thumbnail.component'
+import {
+  EventsListComponent,
+  EventThumbnailComponent,
+  EventService,
+  EventDetailsComponent,
+  CreateEventComponent,
+  EventRouteActivator,
+  EventListResolver
+} from './events/index'
+import { EventsAppComponent } from './events-app.component'
 import { NavBarComponent } from './nav/nav-bar.component'
-import { EventService } from './events/shared/event.service'
-import { ToastrService } from './common/toastr.service';
-import { EventDetailsComponent } from './events/event-details/event-details.component';
+import { ToastrService } from './common/toastr.service'
 import { appRoutes } from './routes'
-import { CreateEventComponent } from './events/create-event.component';
-import { Error404Component } from './errors/404.component';
-import { EventRouteActivator } from './events/event-details/event-route-activator.service';
-import { EventListResolver } from './events/events-list-resolver.service';
+import { Error404Component } from './errors/404.component'
 
 @NgModule({
   imports: [
