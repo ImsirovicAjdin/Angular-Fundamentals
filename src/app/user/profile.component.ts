62cde61d3cac208729a70fd80d8bfcaac7af7a1b
FlowOverstack <ajdika@live.com>
2019-05-14 15:33:02 +0200

    7.6 Creating your first reactive form, pt3

--- src/app/user/profile.component.ts
+++ src/app/user/profile.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core'
 import { FormControl, FormGroup } from '@angular/forms';
 import { AuthService } from './auth.service'
+import { Router } from '@angular/router'
 
 @Component({
   templateUrl: './profile.component.html',
@@ -9,7 +10,7 @@ export class ProfileComponent implements OnInit {
 
   profileForm:FormGroup
 
-  constructor(private authService:AuthService) {}
+  constructor(private router:Router, private authService:AuthService) {}
 
   ngOnInit() {
     let firstname = new FormControl(this.authService.currentUser.firstName)
@@ -19,6 +20,15 @@ export class ProfileComponent implements OnInit {
       lastName: lastName
 
     })
+  }
+
+  saveProfile(formValues) {
+    this.authService.updateCurrentUser(formValues.firstName, formValues.lastName)
+    this.router.navigate(['events'])
+  }
+
+  cancel() {
+    this.router.navigate(['events'])
   }
 
 }
