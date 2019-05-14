8592408a8710ca98ce796d3bbe59277441bf7984
FlowOverstack <ajdika@live.com>
2019-05-14 03:53:02 +0200

    Move events-list.component.ts's template to the template file, reference it via templateUrl

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -2,13 +2,7 @@ import { Component } from '@angular/core'
 
 @Component({
   selector: 'events-list',
-  template: `
-  <div>
-    <h1>Upcoming Angular Events</h1>
-    <hr/>
-    <event-thumbnail [event]="event1"></event-thumbnail>
-  </div>
-  `
+  templateUrl: './events-list.component.html'
 })
 export class EventsListComponent {
   event1 = {
