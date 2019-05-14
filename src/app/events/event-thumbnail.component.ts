a0f846012c328f328959fff3abed607b7ee5505f
FlowOverstack <ajdika@live.com>
2019-05-14 04:07:17 +0200

    Remove redundant comments

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -1,4 +1,3 @@
- // (2) of course, we need to import the input decorator
 import { Component, Input } from '@angular/core'
 
 @Component({
@@ -21,13 +20,5 @@ import { Component, Input } from '@angular/core'
   `]
 })
 export class EventThumbnailComponent {
-    // event:any
-    // (1) If we have the above variable only, there's nothing to
-    // tell Angular to expect a value to be passed into 
-    // our component; that's where the @Input decorator 
-    // comes in
-    @Input() event:any // this 'event' corresponds to our [event] property in (4)
-    // (3) This input decorator tells Angular that this
-    // event will be passed in from another component
-    // which is (4) in event-list.component.html
+    @Input() event:any
 } 
