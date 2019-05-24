59affef1b358149a2c20527c34a60a192106da3c
FlowOverstack <ajdika@live.com>
2019-05-24 17:17:58 +0200

    12.03 Adding jQuery

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,14 +18,13 @@ import {
 } from './events/index'
 import { EventsAppComponent } from './events-app.component'
 import { NavBarComponent } from './nav/nav-bar.component'
-import { TOASTR_TOKEN, Toastr } from './common/toastr.service'
+import { JQ_TOKEN, TOASTR_TOKEN, Toastr, CollapsibleWellComponent } from './common/index'
 import { appRoutes } from './routes'
 import { Error404Component } from './errors/404.component'
 import { AuthService } from './user/auth.service'
-import { CollapsibleWellComponent } from './common/collapsible-well.component';
 
-//declare let toastr:any
 let toastr:Toastr = window['toastr'];
+let jQuery = window['$'];
 
 @NgModule({
   imports: [
